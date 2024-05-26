78bb28491b5b34b1927ac34c507ca0356116914c
ZaninDe <gabriel.zaninde@gmail.com>
2024-05-26 19:19:30 -0300

    add data table and courses grid

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,26 +1,33 @@
 import { Button } from '@/components/ui/button'
 import Link from 'next/link'
-import { Payment, columns } from './_components/columns'
+import { columns } from './_components/columns'
 import { DataTable } from './_components/data-table'
-
-async function getData(): Promise<Payment[]> {
-  // Fetch data from your API here.
-  return [
-    {
-      id: '728ed52f',
-      amount: 100,
-      status: 'pending',
-      email: 'm@example.com',
-    },
-    // ...
-  ]
-}
+import { auth } from '@clerk/nextjs'
+import { redirect } from 'next/navigation'
+import { db } from '@/lib/db'
 
 const CoursesPage = async () => {
-  const data = await getData()
+  const { userId } = auth()
+
+  if (!userId) {
+    return redirect('/')
+  }
+
+  const courses = await db.course.findMany({
+    where: {
+      userId,
+    },
+    orderBy: {
+      createdAt: 'desc',
+    },
+  })
+
   return (
-    <div className="p-6">
-      <DataTable columns={columns} data={data} />
+    <div className="p-6 flex flex-col gap-6">
+      <DataTable columns={columns} data={courses} />
+      <Link href="/create">
+        <Button type="button">Create</Button>
+      </Link>
     </div>
   )
 }
